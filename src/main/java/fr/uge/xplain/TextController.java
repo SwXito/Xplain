ea6aa283fe623a03387fd9ab0f5c0323d9dc94d6
WilliamC57 <willibox571@gmail.com>
2024-10-22 17:11:51 +0200

    debut communication front -> back

--- src/main/java/fr/uge/xplain/TextController.java
+++ src/main/java/fr/uge/xplain/TextController.java
@@ -1,8 +1,11 @@
 package fr.uge.xplain;
 
+import fr.uge.model.LLMService;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.MediaType;
+
+import java.io.IOException;
 import java.util.Objects;
 
 @Path("/api/endpoint")
@@ -11,9 +14,11 @@ public class TextController {
   @POST
   @Consumes(MediaType.TEXT_PLAIN)
   @Produces(MediaType.APPLICATION_JSON)
-  public Response receiveData(String data) {
+  public Response receiveData(String data) throws IOException {
     Objects.requireNonNull(data);
-    return Response.ok(data).build();
+    String reponse = LLMService.start(data);
+
+    return Response.ok(reponse).build();
   }
 
 }

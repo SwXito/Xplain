91cb817ca51936dd8b9ec72ea43bf4c39b8af1fb
dams <touatidamien246@gmail.com>
2024-10-22 23:38:22 +0200

    src/main/java/fr/uge/xplain/Controller.java

--- src/main/java/fr/uge/xplain/Controller.java
+++ src/main/java/fr/uge/xplain/Controller.java
@@ -3,12 +3,14 @@ package fr.uge.xplain;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.MediaType;
+
 import java.util.Objects;
 
-@Path("/api/endpoint")
-public class TextController {
+@Path("/api")
+public class Controller {
 
   @POST
+  @Path("/endpoint")
   @Consumes(MediaType.TEXT_PLAIN)
   @Produces(MediaType.APPLICATION_JSON)
   public Response receiveData(String data) {
@@ -16,4 +18,13 @@ public class TextController {
     return Response.ok(data).build();
   }
 
+  @POST
+  @Path("/model")
+  @Consumes(MediaType.TEXT_PLAIN)
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response receiveModel(String model) {
+    Objects.requireNonNull(model);
+    System.out.println("Downloading " + model + "...");
+    return Response.ok(model).build();
+  }
 }

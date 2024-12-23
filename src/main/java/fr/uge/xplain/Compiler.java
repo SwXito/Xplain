1783a150a6e45df9460b045d4700b1c02da7d561
SwXito <damientouati@Mac.lan>
2024-12-23 21:15:50 +0100

    nothing

--- src/main/java/fr/uge/xplain/Compiler.java
+++ src/main/java/fr/uge/xplain/Compiler.java
@@ -8,12 +8,12 @@ import java.io.*;
 import java.util.List;
 import java.util.Objects;
 
-public class Compiler {
+public final class Compiler {
 
   public static String compile(String input) throws IOException {
     Objects.requireNonNull(input);
     var fileName = Parser.getClassName(input).orElse("");
-    if(fileName.isEmpty()){
+    if (fileName.isEmpty()) {
       return "Compilation failed: No class name found in input";
     }
     var sourceObject = new JavaSourceFromString(fileName, input);
@@ -23,23 +23,23 @@ public class Compiler {
     var outputWriter = new StringWriter();
     var errorWriter = new StringWriter();
     var success = compiler.getTask(new PrintWriter(outputWriter), fileManager, diagnostic -> {
-      errorWriter.write(diagnostic.toString() + "\n");}, null, null, compilationUnits).call();
+      errorWriter.write(diagnostic.toString() + "\n");
+    }, null, null, compilationUnits).call();
     fileManager.close();
     deleteUselessFiles(new File(fileName + ".class"));
     return constructCompilerMessage(outputWriter.toString(), errorWriter.toString(), success);
   }
 
-  private static String constructCompilerMessage(String output, String error, boolean success){
-    var res = "Compilation result:\n";
-    if(success){
-      return res + "Compilation successful\n" + output;
+  private static String constructCompilerMessage(String output, String error, boolean success) {
+    if (success) {
+      return "Compilation successful\n" + output;
     }
-    return  res + "Compilation failed\n" + error + output;
+    return "Compilation failed\n" + error + output;
   }
 
   private static void deleteUselessFiles(File... files) {
     for (File file : files) {
-      if(file.exists()){
+      if (file.exists()) {
         file.delete();
       }
     }

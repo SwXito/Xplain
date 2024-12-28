f6954814498fd548667c7d4e387470040497c0eb
WilliamC57 <willibox571@gmail.com>
2024-12-28 16:33:11 +0100

    changement de llm

--- src/main/java/fr/uge/model/LLMService.java
+++ src/main/java/fr/uge/model/LLMService.java
@@ -15,31 +15,31 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.jboss.resteasy.reactive.RestStreamElementType;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.Objects;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 @Path("/api/generate")
 public class LLMService {
-  private final ReentrantLock lock = new ReentrantLock();
-  private String modelPath = "mistral-7b-instruct-v0.2.Q5_K_S.gguf";
+  private final ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
+  private final Lock readLock = rwl.readLock();
+  private final Lock writeLock = rwl.writeLock();
+  private String modelPath = "mistral-7b-instruct-v0.2.Q5_K_S.gguf"; // critique
   private final HashMap<String, ModelParameters> modelsParams = new HashMap<>();
-  private final String prompt = "You are Llama an assistant that provide advice for java programmer\n" +
-    "Llama is helpful, and suggest only 5 things upgradable, with short sentences" +
-    "Once the User submit a java class with a compiler message.\n\n";
-  //private InferenceParameters inferParams = null;
-  private final ConcurrentHashMap<String, LlamaModel> models = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, LlamaModel> models = new ConcurrentHashMap<>(); // critique
   private final LinkedBlockingQueue<RequestData> queue = new LinkedBlockingQueue<>();
-  //private long current;
-
   /***
    * This record is used to manage the request data
    * @param id the id of the in the database
    * @param parameters paramaters required to generate the response
+   * @param model name of the model currently used
    */
   private record RequestData(long id, InferenceParameters parameters, String model){}
   @Inject
@@ -49,14 +49,14 @@ public class LLMService {
     ModelDownloader.map.forEach((k, v) -> modelsParams.put(v.modelName(),
       new ModelParameters()
         .setModelFilePath("models/" + v.modelName())
-        .setNGpuLayers(10)
-              .setNSequences(5)
+        .setNGpuLayers(35)
+              .setNSequences(5) // genere jusqu'à 5 requete simultané
               .setNParallel(5)));
     Thread.ofPlatform().start(() -> {
-      lock.lock();
-        //ModelDownloader.map.forEach((k, v) -> { models.put(v.modelName(), null /*new LlamaModel(modelsParams.get(v.modelName()))*/);});
+      writeLock.lock();
+        //ModelDownloader.map.forEach((k, v) -> { models.put(v.modelName(), /*new LlamaModel(modelsParams.get(v.modelName()))*/);});
         models.put("mistral-7b-instruct-v0.2.Q5_K_S.gguf", new LlamaModel(modelsParams.get("mistral-7b-instruct-v0.2.Q5_K_S.gguf")));
-      lock.unlock();
+      writeLock.unlock();
     });
   }
 
@@ -64,14 +64,26 @@ public class LLMService {
   public void newRequest(String classPrompt, String compilerOutputPrompt, long id) {
     Objects.requireNonNull(classPrompt);
     Objects.requireNonNull(compilerOutputPrompt);
-    var prompt = this.prompt + "\nUser: " + classPrompt + "\n\n" + "And this is the answer of the compiler :" +
-      compilerOutputPrompt + "\n\nCan you give me some advices to improve my class ?";
+    var prompt = getPrompt(classPrompt, compilerOutputPrompt);
+    readLock.lock(); // pour le modelPath
+    queue.offer(new RequestData(id,new InferenceParameters(prompt)
+      .setTemperature(0.5f)
+      .setPenalizeNl(true)
+      .setMiroStat(MiroStat.V2)
+      .setStopStrings("User:"), modelPath));
+    readLock.unlock();
+  }
+
+  private static @NotNull String getPrompt(String classPrompt, String compilerOutputPrompt) {
+    String basePrompt = """
+            You are Llama an assistant that provide advice for java programmer
+            Llama is helpful, and suggest only 5 things upgradable, with short sentences
+            Once the User submit a java class with a compiler message.
+            """;
+    var prompt = basePrompt + "\nUser: " + classPrompt + "\n\n" + "And this is the answer of the compiler :" +
+            compilerOutputPrompt + "\n\nCan you give me some advices to improve my class ?";
     prompt += "\nLama: ";
-      queue.offer(new RequestData(id,new InferenceParameters(prompt)
-        .setTemperature(0.5f)
-        .setPenalizeNl(true)
-        .setMiroStat(MiroStat.V2)
-        .setStopStrings("User:"), modelPath));
+    return prompt;
   }
 
   @Path("response")
@@ -80,24 +92,27 @@ public class LLMService {
   public Multi<SimpleBoxer> generateCompleteResponse() {
     return Multi.createFrom().emitter(emitter -> {
       Thread.ofPlatform().start(() -> {
-          var data = queue.poll();
-          if (data == null || models.get(data.model) == null) {
-          emitter.complete(); // nien a envoyer
+        readLock.lock(); // pour le model
+      try {
+        var data = queue.poll();
+        if (data == null || models.get(data.model) == null) {
+          emitter.complete(); // rien a envoyer
         } else {
           var model = models.get(data.model);
-          emitter.emit(new SimpleBoxer("start", "")); // debut de la generation de reponse
+          emitter.emit(new SimpleBoxer("start", "")); // debut de la generation de la reponse
           var builder = new StringBuilder();
 
           for (var output : model.generate(data.parameters)) {
             emitter.emit(new SimpleBoxer("token", output.toString()));
             builder.append(output.toString());
           }
-          dbService.updateLlmResponse(data.id, builder.toString());
+          dbService.updateLlmResponse(data.id, builder.toString()); // ajout de la reponse complete dans la bdd
           emitter.emit(new SimpleBoxer("end", "")); // fin de la genereation
           emitter.complete();
-          //inferParams = null; // nettoie les parametres
-          //current = null;  // nettoie la reference de la bdd
         }
+      } finally {
+        readLock.unlock();
+      }
       });
     });
   }
@@ -108,15 +123,15 @@ public class LLMService {
   @Produces(MediaType.APPLICATION_JSON)
   public Response receiveModel(String model) {
     Objects.requireNonNull(model);
-    lock.lock();
+    writeLock.lock(); // pour le model
     try {
       System.out.println(model);
-      //models.get(modelPath).close(); // ferme le vieux model
+      models.get(modelPath).close(); // ferme le vieux model
       models.put(model, new LlamaModel(modelsParams.get(model))); // lance le nouveau model
       modelPath = model; // nouveau model path
       return Response.ok(model).build();
     } finally {
-      lock.unlock();
+      writeLock.unlock();
     }
   }
 
